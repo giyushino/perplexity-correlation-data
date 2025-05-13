663790938a4bfe27a6fed833c8a43b07cd0dc558
giyushino <allanzhang440@gmail.com>
2025-05-13 13:03:06 -0700

    train function

---
+++ perplexity-correlation-data/train/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate perplexity
+export CUDA_VISIBLE_DEVICES=3,4,5,6,7
+torchrun --nproc-per-node=5 train.py
+
+
--- perplexity-correlation-data/train/train.py
+++ perplexity-correlation-data/train/train.py
@@ -4,25 +4,26 @@ import wandb
 from datasets import load_dataset
 from transformers import (Trainer, TrainingArguments,
                           GPTNeoXForCausalLM, AutoConfig, 
-                          AutoTokenizer
+                          AutoTokenizer, DataCollatorForLanguageModeling
                           )
 
 def tokenize_dataset(example):
-    tokens = tokenizer(example["text"], truncation=True, padding=False)
+    tokens = tokenizer(example["text"], truncation=True, padding=True, max_length=2048)
     tokens["labels"] = tokens["input_ids"].copy()
     return tokens
 
-def train(model_size = "EleutherAI/pythia-160m-deduped"):
+def train(dataset, tokenizer, model_size = "EleutherAI/pythia-160m-deduped"):
     #dataset = load_dataset("json", data_files = dataset_path) 
-    model = GPTNeoXForCausalLM.from_pretrained(model_size)
-    wandb.init(project="perplexity-llm-pretraining")
     # Reinitialize model to have random weights, training from scratch 
     config = AutoConfig.from_pretrained(model_size)
     reinit_model = GPTNeoXForCausalLM(config)
+    wandb.init(project="perplexity-llm-pretraining", 
+               config = vars(config)) 
     # Hyperparameters from paper (Appendix G)
     training_args = TrainingArguments(
-        output_dir= os.path.join(base_path, "perplexity-correlation-data-testing", "data", "model_weights"), 
-        per_device_train_batch_size=128, 
+        output_dir= "/home/allanz/perplexity-correlation-data/data/model_weights/", 
+        run_name="test_10k", 
+        per_device_train_batch_size=8, 
         learning_rate = 5e-3, 
         warmup_ratio=0.1,
         weight_decay=0.1,
@@ -31,26 +32,30 @@ def train(model_size = "EleutherAI/pythia-160m-deduped"):
         bf16=True,
         gradient_accumulation_steps=1,
         report_to="wandb",   
-        #ddp_backend="nccl",    
+        ddp_backend="nccl",    
     )
     
     trainer = Trainer(
         model = reinit_model, 
         args = training_args, 
-        dataset = dataset  
-    )
+        train_dataset = dataset, 
+        data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False) 
 
-    return 0
+    )
+    trainer.train()
 
 
 if __name__ == "__main__":
-    """
     tokenizer = AutoTokenizer.from_pretrained(
     "EleutherAI/pythia-70m-deduped",
     revision="step3000",
     cache_dir="./pythia-70m-deduped/step3000",
     )
-    """
-    #dataset = load_dataset("json", data_files="/home/allanz/perplexity-correlation-data/data/selected_subsets/preselect.jsonl", split="train")
-    #tokenized_dataset = dataset.map(tokenize_dataset, batched=True)
-    #print(tokenized_dataset)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token 
+    dataset = load_dataset("json", data_files = "/home/allanz/perplexity-correlation-data/data/selected_subsets/random_10k.jsonl", split="train")
+    dataset = dataset.select(range(1000))
+    dataset = dataset.map(tokenize_dataset, batched=True)
+    dataset = dataset.remove_columns(["text", "url", "file_num", "index", "label" ,"probability", "token_length"])
+    print(dataset)
+    train(dataset, tokenizer)
