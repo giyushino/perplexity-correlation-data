0281023ff8ce0bd1069441f18ddf74f7ee49f407
giyushino <allanzhang440@gmail.com>
2025-05-12 22:09:38 -0700

    update train

--- perplexity-correlation-data/train/train.py
+++ perplexity-correlation-data/train/train.py
@@ -1,15 +1,21 @@
+#conda_env: perplexity
 import os
+import wandb
 from datasets import load_dataset
 from transformers import (Trainer, TrainingArguments,
-                          GPTNeoXForCausalLM, AutoConfig
+                          GPTNeoXForCausalLM, AutoConfig, 
+                          AutoTokenizer
                           )
 
-base_path = os.environ["WORK"]
+def tokenize_dataset(example):
+    tokens = tokenizer(example["text"], truncation=True, padding=False)
+    tokens["labels"] = tokens["input_ids"].copy()
+    return tokens
 
-def train(dataset_path, model_size = "EleutherAI/pythia-160m-deduped"):
-    dataset = load_dataset("json", dataset_path) 
+def train(model_size = "EleutherAI/pythia-160m-deduped"):
+    #dataset = load_dataset("json", data_files = dataset_path) 
     model = GPTNeoXForCausalLM.from_pretrained(model_size)
-
+    wandb.init(project="perplexity-llm-pretraining")
     # Reinitialize model to have random weights, training from scratch 
     config = AutoConfig.from_pretrained(model_size)
     reinit_model = GPTNeoXForCausalLM(config)
@@ -38,4 +44,13 @@ def train(dataset_path, model_size = "EleutherAI/pythia-160m-deduped"):
 
 
 if __name__ == "__main__":
-    train()
+    """
+    tokenizer = AutoTokenizer.from_pretrained(
+    "EleutherAI/pythia-70m-deduped",
+    revision="step3000",
+    cache_dir="./pythia-70m-deduped/step3000",
+    )
+    """
+    #dataset = load_dataset("json", data_files="/home/allanz/perplexity-correlation-data/data/selected_subsets/preselect.jsonl", split="train")
+    #tokenized_dataset = dataset.map(tokenize_dataset, batched=True)
+    #print(tokenized_dataset)
